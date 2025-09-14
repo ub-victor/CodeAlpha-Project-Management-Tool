f3f85d0c01597ed8cdf32498956830e7e240b825
ub-victor <victoireushindi371@gmail.com>
2025-09-14 23:10:12 +0200

    Initial commit

---
+++ backend/models/Comment.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  task: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
